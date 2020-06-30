c89a9a1b3c9d18619d8188ccd92cf5d569da5c0d
Stefan Bender <stefan.bender@ntnu.no>
2020-06-30 21:16:54 +0200

    dataset: Test LST broadcasting and setting
    Calculating the local solar time based on the inputs can be challenging,
    adds some tests to make sure they propagate properly to the correct
    shapes and values.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf-8
 import datetime as dt
+import numpy as np
 import pytest
 
 from nrlmsise00.dataset import msise_4d
@@ -29,3 +30,48 @@ def test_dataset(ap, lon, lst):
 		lst=lst,
 	)
 	assert ds
+
+
+@pytest.mark.parametrize(
+	"lon, lst, lst_expected", [
+		(
+			0,
+			None,
+			np.array([8., 16.]).reshape(2, 1),
+		),
+		(
+			[30, 150],
+			None,
+			np.array([[10., 18.], [18., 26.]]).reshape(2, 2),
+		),
+		(
+			[30, 120, 150],
+			None,
+			np.array([[10., 16., 18.], [18., 24., 26.]]),
+		),
+		(
+			[30, 120, 150],
+			[10., 16.],  # same shape as "time"
+			np.array([[10., 10., 10.], [16., 16., 16.]]),
+		),
+		(
+			[30, 120, 150],
+			[10., 16., 18.],  # same shape as "lon"
+			np.array([[10., 16., 18.], [10., 16., 18.]]),
+		),
+	]
+)
+def test_lst(lon, lst, lst_expected):
+	# checks that local time works as intended
+	ds = msise_4d(
+		[dt.datetime(2009, 6, 21, 8), dt.datetime(2009, 12, 21, 16)],
+		[400, 200, 100],  # alt
+		[60, 30, 0, -30, -60],  # g_lat
+		lon,    # g_long
+		150,    # f107A
+		150,    # f107
+		4,      # ap
+		lst=lst,
+	)
+	assert ds
+	np.testing.assert_allclose(ds.lst.values, lst_expected)
